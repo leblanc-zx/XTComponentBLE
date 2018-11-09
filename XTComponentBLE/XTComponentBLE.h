32d72d723c8f2ca0a7bf4012c601e4e25fcb4d7c
351706354@qq.com <351706354@qq.com>
2018-11-09 09:37:57 +0800

    Initial Commit

---
+++ XTComponentBLE/XTComponentBLE.h
@@ -0,0 +1,19 @@
+//
+//  XTComponentBLE.h
+//  XTComponentBLE
+//
+//  Created by apple on 2018/11/9.
+//  Copyright © 2018年 新天科技股份有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XTComponentBLE.
+FOUNDATION_EXPORT double XTComponentBLEVersionNumber;
+
+//! Project version string for XTComponentBLE.
+FOUNDATION_EXPORT const unsigned char XTComponentBLEVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XTComponentBLE/PublicHeader.h>
+
+
